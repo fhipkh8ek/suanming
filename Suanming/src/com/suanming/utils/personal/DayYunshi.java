2f08786d94c63ebedb9b2bd5d9b120744ec184bc
fhipkh8ek <fengzhongza199941@126.com>
2015-01-27 11:16:29 +0800

    个人模块-每日运势

---
+++ Suanming/src/com/suanming/utils/personal/DayYunshi.java
@@ -0,0 +1,20 @@
+package com.suanming.utils.personal;
+public class DayYunshi {
+
+	/**
+	 * 
+	* @Title: getColor
+	* @Description: TODO
+	* @param: @return
+	* @return: String   
+	* @autor:weixin
+	* @time:2015年1月27日 上午11:13:27
+	 */
+	public String getColor() {
+		
+		
+		return ""; 
+	}
+	
+	
+}
