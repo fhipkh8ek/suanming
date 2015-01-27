c42c266e3c713a8e068275b4c1e25a81fd95269c
fhipkh8ek <fengzhongza199941@126.com>
2015-01-27 10:53:27 +0800

    更新所有

--- Suanming/src/com/suanming/demo/DemoTest.java
+++ Suanming/src/com/suanming/demo/DemoTest.java
@@ -1,7 +1,14 @@
 package com.suanming.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class DemoTest {
-	public static void main(String[] args) {
-		System.out.println("dsadsa");
+	
+	@RequestMapping("index.htm")
+	public String index() {
+		return "index";
 	}
+	
 }
