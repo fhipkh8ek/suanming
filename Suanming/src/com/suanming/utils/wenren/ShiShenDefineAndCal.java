0fa413685217c517abcdfc830eb9da0a2bb7bd09
fhipkh8ek <fengzhongza199941@126.com>
2015-01-29 11:36:21 +0800

    添加

---
+++ Suanming/src/com/suanming/utils/wenren/ShiShenDefineAndCal.java
@@ -0,0 +1,32 @@
+package com.suanming.utils.wenren;
+
+import com.suanming.utils.wenren.vo.BaZiShiShenValue;
+
+public class ShiShenDefineAndCal {
+
+	public BaZiShiShenValue doDefineAndCal(String bzInfo) {
+
+		BaZiShiShenValue bsv = new BaZiShiShenValue();
+		String nianGan = bzInfo.substring(0, 1);
+		String nianZhi = bzInfo.substring(1, 2);
+		String yueGan = bzInfo.substring(2, 3);
+		String yueZhi = bzInfo.substring(3, 4);
+		String riGan = bzInfo.substring(4, 5);
+		String riZhi = bzInfo.substring(5, 6);
+		String shiGan = bzInfo.substring(6, 7);
+		String shiZhi = bzInfo.substring(7, 8);
+
+	}
+
+	public String doDefine(String wuxing, String rigan) {
+
+		String shishen = "";
+
+		
+	
+		
+		
+		return shishen;
+	}
+
+}
