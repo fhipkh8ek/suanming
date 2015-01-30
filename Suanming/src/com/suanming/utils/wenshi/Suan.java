cc510aa56747415e4cc4966df5ffd7451d54d465
fhipkh8ek <fengzhongza199941@126.com>
2015-01-30 16:10:31 +0800

    update

--- Suanming/src/com/suanming/utils/wenshi/Suan.java
+++ Suanming/src/com/suanming/utils/wenshi/Suan.java
@@ -7,8 +7,8 @@ public class Suan {
 
 	public static void main(String[] args) {
 
-		SuanGuaInfo suanGuaInfo = new SuanGuaInfo("未",11, 8, 7, "辛", "酉",
-				"er", "戌");
+		SuanGuaInfo suanGuaInfo = new SuanGuaInfo("午", 11, 3, 7, "丙", "午",
+				"er", "丑");
 
 		SuoDeGua sdg = new SuoDeGua();
 
@@ -19,20 +19,35 @@ public class Suan {
 		sdg = sgd.sanGuan();
 
 		pailie = sdg.pailieAsse();
-		
-		 if (pailie.split("@")[3].equals("kong")){
-			
-			 
-			 
-			 System.out.println("你所要问的事目前吉凶未定，请您改日再测。");
-			 
+
+		if (pailie.split("@")[3].equals("kong")) {
+
+			System.out.println("你所要问的事目前吉凶未定，请您改日再测。");
+
 		}
-		
-		 
-		 else{
-			 
-			 System.out.println(PropertyUtil.getValue(pailie, "wenshi.properties"));
-		 }
-	
+
+		else {
+
+			String[] result = new String[7];
+			result = PropertyUtil.getValue(pailie, "wenshi.properties").split(
+					"@");
+			String jielun = result[0];
+			String guiren = result[1];
+			String xiaoren = result[2];
+			String jifang = result[3];
+			String xiongfang = result[4];
+			String jishi = result[5];
+			String xiongshi = result[6];
+
+			System.out.println("结论:" + jielun);
+			System.out.println("贵人:" + guiren);
+			System.out.println("小人:" + xiaoren);
+			System.out.println("吉方:" + jifang);
+			System.out.println("凶方:" + xiongfang);
+			System.out.println("吉时:" + jishi);
+			System.out.println("凶时:" + xiongshi);
+
+		}
+
 	}
 }
