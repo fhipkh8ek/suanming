0339612f4e837a89e2516e5d013b52d5b1303a13
fhipkh8ek <fengzhongza199941@126.com>
2015-01-30 17:51:44 +0800

    修正

--- Suanming/src/com/suanming/controller/MainController.java
+++ Suanming/src/com/suanming/controller/MainController.java
@@ -2,8 +2,13 @@ package com.suanming.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.suanming.utils.personal.DayYunshi;
+import com.suanming.utils.personal.pojo.PersonalYun;
 
 @Controller
 public class MainController {
@@ -12,17 +17,17 @@ public class MainController {
 	 * 
 	* @Title: getPersonInfo
 	* @Description: TODO
-	* @return
 	* @autor:weixin
 	* @time:2015年1月29日 下午5:20:31
 	 */
-	@RequestMapping("pinfo.htm")
+	@RequestMapping("index.htm")
 	public String getPersonInfo(HttpServletRequest request) {
 		String inDate = request.getParameter("inDate");
 		String inHour = request.getParameter("inHour");
-		
-		System.out.println(inDate+":"+inHour);
-		
+		if (StringUtils.isNotBlank(inDate) && StringUtils.isNotBlank(inHour)) {
+			request.getSession().setAttribute("inDate", inDate);
+			request.getSession().setAttribute("inHour", Integer.parseInt(inHour));
+		}
 		return "index";
 	}
 	
@@ -31,6 +36,4 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
-	
-	
 }
