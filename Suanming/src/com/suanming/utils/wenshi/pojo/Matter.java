3295efadeeadf192121431c8d9a347afd853d830
fhipkh8ek <fengzhongza199941@126.com>
2015-01-30 15:54:42 +0800

    inti

---
+++ Suanming/src/com/suanming/utils/wenshi/pojo/Matter.java
@@ -0,0 +1,67 @@
+package com.suanming.utils.wenshi.pojo;
+
+
+public class Matter {
+	
+	//评价
+	private String assess = "";
+	//吉时
+	private String goodTime = "";
+	//凶时
+	private String badTime = "";
+	//贵人
+	private String goodGuy = "";
+	//小人
+	private String badGuy  = "";
+	//其他
+	private String other = "";
+	
+	public String getOther() {
+		return other;
+	}
+
+	public void setOther(String other) {
+		this.other = other;
+	}
+
+	public String getAssess() {
+		return assess;
+	}
+
+	public void setAssess(String assess) {
+		this.assess = assess;
+	}
+
+	public String getGoodTime() {
+		return goodTime;
+	}
+
+	public void setGoodTime(String goodTime) {
+		this.goodTime = goodTime;
+	}
+
+	public String getBadTime() {
+		return badTime;
+	}
+
+	public void setBadTime(String badTime) {
+		this.badTime = badTime;
+	}
+
+	public String getGoodGuy() {
+		return goodGuy;
+	}
+
+	public void setGoodGuy(String goodGuy) {
+		this.goodGuy = goodGuy;
+	}
+
+	public String getBadGuy() {
+		return badGuy;
+	}
+
+	public void setBadGuy(String badGuy) {
+		this.badGuy = badGuy;
+	}
+}
+
