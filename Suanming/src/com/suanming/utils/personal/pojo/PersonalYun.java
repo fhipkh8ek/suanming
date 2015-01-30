650daac5578592ed60c14e5722671962c7297712
fhipkh8ek <fengzhongza199941@126.com>
2015-01-30 15:56:06 +0800

    init

---
+++ Suanming/src/com/suanming/utils/personal/pojo/PersonalYun.java
@@ -0,0 +1,74 @@
+package com.suanming.utils.personal.pojo;
+
+public class PersonalYun {
+	
+	private String goodTime = "";
+	
+	private String badTime = "";
+	
+	private String isGood = "";
+	
+	private String isBad = "";
+	
+	private String color = "";
+	
+	private String direction = "";
+
+	private String chineseTime = "";
+	
+	public String getChineseTime() {
+		return chineseTime;
+	}
+
+	public void setChineseTime(String chineseTime) {
+		this.chineseTime = chineseTime;
+	}
+
+	public String getGoodTime() {
+		return goodTime;
+	}
+
+	public void setGoodTime(String goodTime) {
+		this.goodTime = goodTime;
+	}
+
+	public String getBadTime() {
+		return badTime;
+	}
+
+	public void setBadTime(String badTime) {
+		this.badTime = badTime;
+	}
+
+	public String getIsGood() {
+		return isGood;
+	}
+
+	public void setIsGood(String isGood) {
+		this.isGood = isGood;
+	}
+
+	public String getIsBad() {
+		return isBad;
+	}
+
+	public void setIsBad(String isBad) {
+		this.isBad = isBad;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+}
