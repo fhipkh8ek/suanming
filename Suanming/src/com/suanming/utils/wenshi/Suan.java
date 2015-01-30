bf7ccba094cedee550ce8745b32154106b6150f2
fhipkh8ek <fengzhongza199941@126.com>
2015-01-30 17:55:19 +0800

    修正版

--- Suanming/src/com/suanming/utils/wenshi/Suan.java
+++ Suanming/src/com/suanming/utils/wenshi/Suan.java
@@ -1,53 +1,66 @@
 package com.suanming.utils.wenshi;
 
+import java.text.ParseException;
+
+
 import com.suanming.utils.bagua.*;
+import com.suanming.utils.base.BaseUtils;
 import com.suanming.utils.base.PropertyUtil;
+import com.suanming.utils.bazi.BaziInfo;
+import com.suanming.utils.wenshi.pojo.Matter;
 
 public class Suan {
 
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * @Title: getMatterResult
+	 * @Description: 获取结果
+	 * @param inDate
+	 * @throws ParseException
+	 * @autor:weixin
+	 * @time:2015年1月30日 下午4:14:28
+	 */
+	public Matter getMatterResult(String type) throws ParseException {
+		Matter matter = new Matter();
+		String chineseTime = new BaziInfo().getBaziInfo(
+				BaseUtils.getNowTime(2), 1);
+		// 年支
+		String nianzhi = chineseTime.substring(1, 2);
+		// 阴历
+		String chinese = new BaziInfo().getChineseDate(BaseUtils.getNowTime(2));
+		String chineseMonth = chinese.split(",")[0];
+		String chineseDay = chinese.split(",")[1];
+		// 时辰
+		String time = BaseUtils.dizhiToNum(BaseUtils.getNowTime(4));
+		// 日干支
+		String riGan = chineseTime.substring(4, 5);
+		String riZhi = chineseTime.substring(5, 6);
+		// 月支
+		String yueZhi = chineseTime.substring(3, 4);
 
-		SuanGuaInfo suanGuaInfo = new SuanGuaInfo("午", 11, 3, 7, "丙", "午",
-				"er", "丑");
-
+		
+		//取断语
+		SuanGuaInfo suanGuaInfo = new SuanGuaInfo(nianzhi,
+				Integer.parseInt(chineseMonth), Integer.parseInt(chineseDay),
+				Integer.parseInt(time), riGan, riZhi, type, yueZhi);
 		SuoDeGua sdg = new SuoDeGua();
-
 		String pailie;
-
 		SanGuaDingWei sgd = new SanGuaDingWei(suanGuaInfo);
-
 		sdg = sgd.sanGuan();
-
 		pailie = sdg.pailieAsse();
-
 		if (pailie.split("@")[3].equals("kong")) {
-
-			System.out.println("你所要问的事目前吉凶未定，请您改日再测。");
-
+			matter.setOther("你所要问的事目前吉凶未定，请您改日再测。");
 		}
-
 		else {
-
-			String[] result = new String[7];
-			result = PropertyUtil.getValue(pailie, "wenshi.properties").split(
-					"@");
-			String jielun = result[0];
-			String guiren = result[1];
-			String xiaoren = result[2];
-			String jifang = result[3];
-			String xiongfang = result[4];
-			String jishi = result[5];
-			String xiongshi = result[6];
-//
-			System.out.println("结论:" + jielun);
-			System.out.println("贵人:" + guiren);
-			System.out.println("小人:" + xiaoren);
-			System.out.println("吉方:" + jifang);
-			System.out.println("凶方:" + xiongfang);
-			System.out.println("吉时:" + jishi);
-			System.out.println("凶时:" + xiongshi);
-
+			String[] result = PropertyUtil.getValue(pailie, "wenshi.properties").split("@");
+			matter.setAssess(result[0]);
+			matter.setGoodGuy(result[1]);
+			matter.setBadGuy(result[2]);
+			matter.setGoodDirect(result[3]);
+			matter.setBadDirect(result[4]);
+			matter.setGoodTime(result[5]);
+			matter.setBadTime(result[6]);
 		}
-
+		return matter;
 	}
 }
