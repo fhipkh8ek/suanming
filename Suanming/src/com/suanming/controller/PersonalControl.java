868a228c4bf2982b94d372e186fe61eeb72d175c
fhipkh8ek <fengzhongza199941@126.com>
2015-02-03 09:55:07 +0800

    update

--- Suanming/src/com/suanming/controller/PersonalControl.java
+++ Suanming/src/com/suanming/controller/PersonalControl.java
@@ -2,8 +2,6 @@ package com.suanming.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.hibernate.envers.Audited;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +11,13 @@ import com.suanming.utils.personal.DayYunshi;
 @Controller
 @RequestMapping("person")
 public class PersonalControl {
-	
+
 	@RequestMapping("perDay.htm")
-	public String getDayYunshi(HttpServletRequest request,Model model) {
+	public String getDayYunshi(HttpServletRequest request, Model model) {
 		String inDate = (String) request.getSession().getAttribute("inDate");
 		Integer inHour = (Integer) request.getSession().getAttribute("inHour");
-		model.addAttribute("person", new DayYunshi().getPersonalYun(inDate, inHour));
+		model.addAttribute("person",
+				new DayYunshi().getPersonalYun(inDate, inHour));
 		return "personal/info";
 	}
 }
