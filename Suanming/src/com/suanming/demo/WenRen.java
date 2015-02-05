149b5d4a41eb720dcc5d0af2e2b737907b50bba9
fhipkh8ek <fengzhongza199941@126.com>
2015-02-05 16:40:28 +0800

    edit

--- Suanming/src/com/suanming/demo/WenRen.java
+++ Suanming/src/com/suanming/demo/WenRen.java
@@ -6,19 +6,16 @@ import com.suanming.utils.wenren.Result;
 public class WenRen {
 
 	public static void main(String[] args) {
-		
+
 		BaziInfo baziInfo = new BaziInfo();
-		//八字
-		String wxbz = baziInfo.getBaziInfo("1992-12-07",11);
-		
-		String lsbz = baziInfo.getBaziInfo("1990-2-28",9);
-		
-		
+		// 八字
+		String wxbz = baziInfo.getBaziInfo("1992-12-07", 11);
+
+		String lsbz = baziInfo.getBaziInfo("1990-2-28", 9);
+
 		Result result = new Result();
-		String duanyu = result.getDuanyu(wxbz, lsbz,"");
-		
+		String duanyu = result.getDuanyu(wxbz, lsbz, "");
+
 		System.out.println(duanyu);
-		
 	}
-	
 }
