81f278ce01b91cad099ba362aa130dbf2fa397df
fhipkh8ek <fengzhongza199941@126.com>
2015-02-05 09:53:49 +0800

    update

--- Suanming/src/com/suanming/controller/WenrenController.java
+++ Suanming/src/com/suanming/controller/WenrenController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import sun.launcher.resources.launcher;
 
 import com.suanming.utils.bazi.BaziInfo;
 import com.suanming.utils.wenren.Result;
