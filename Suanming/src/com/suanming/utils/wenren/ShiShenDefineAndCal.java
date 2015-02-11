022319973a7aa783756ac47f28dbd5ab883a1867
fhipkh8ek <fengzhongza199941@126.com>
2015-02-11 11:54:33 +0800

    Edit

--- Suanming/src/com/suanming/utils/wenren/ShiShenDefineAndCal.java
+++ Suanming/src/com/suanming/utils/wenren/ShiShenDefineAndCal.java
@@ -11,27 +11,30 @@ public class ShiShenDefineAndCal {
 		BaZiShiShenValue bsv = new BaZiShiShenValue();
 		ShiShenOp sOp = new ShiShenOp();
 
-		String nianGan = PropertyUtil.getValue(bzInfo.substring(0, 1), "tianganPinyin.properties");
-		String nianZhi = PropertyUtil.getValue(bzInfo.substring(1, 2), "dizhiPinyin.properties");
-		String yueGan = PropertyUtil.getValue(bzInfo.substring(2, 3), "tianganPinyin.properties");
-		String yueZhi = PropertyUtil.getValue(bzInfo.substring(3, 4), "dizhiPinyin.properties");
-		String riGan = PropertyUtil.getValue(bzInfo.substring(4, 5), "tianganPinyin.properties");
-		String riZhi = PropertyUtil.getValue(bzInfo.substring(5,6), "dizhiPinyin.properties");
-		String shiGan = PropertyUtil.getValue(bzInfo.substring(6, 7), "tianganPinyin.properties");
-		String shiZhi = PropertyUtil.getValue(bzInfo.substring(7, 8), "dizhiPinyin.properties");
+		String nianGan = PropertyUtil.getValue(bzInfo.substring(0, 1),
+				"tianganPinyin.properties");
+		String nianZhi = PropertyUtil.getValue(bzInfo.substring(1, 2),
+				"dizhiPinyin.properties");
+		String yueGan = PropertyUtil.getValue(bzInfo.substring(2, 3),
+				"tianganPinyin.properties");
+		String yueZhi = PropertyUtil.getValue(bzInfo.substring(3, 4),
+				"dizhiPinyin.properties");
+		String riGan = PropertyUtil.getValue(bzInfo.substring(4, 5),
+				"tianganPinyin.properties");
+		String riZhi = PropertyUtil.getValue(bzInfo.substring(5, 6),
+				"dizhiPinyin.properties");
+		String shiGan = PropertyUtil.getValue(bzInfo.substring(6, 7),
+				"tianganPinyin.properties");
+		String shiZhi = PropertyUtil.getValue(bzInfo.substring(7, 8),
+				"dizhiPinyin.properties");
 
 		String[] gan = new String[3];
-		String[] zhi = new String[3];
 
 		gan[0] = nianGan;
 		gan[1] = yueGan;
 		gan[2] = shiGan;
 
-		zhi[0] = nianZhi;
-		zhi[1] = riZhi;
-		zhi[2] = shiZhi;
-
-		bsv = sOp.doReturn(gan, riGan, zhi, yueZhi);
+		bsv = sOp.doReturn(gan, riGan, nianZhi, yueZhi, riZhi, shiZhi);
 		return bsv;
 	}
 }
